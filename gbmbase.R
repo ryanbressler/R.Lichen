0769ef6ea1aef14c68d59fa07ef3b3a241e7efb0
Ryan Bressler <ryanbressler@gmail.com>
2009-09-21 23:12:38 -0700

    legend, links

--- gbmbase.R
+++ gbmbase.R
@@ -12,11 +12,11 @@ shapes$complex = "TRIANGLE_UP"
 shapes$lipid = "DIAMOND"
 
 arrowtype = Object()
-arrowtype$inhibits = "LINES"
+arrowtype$inhibits = "TRIANGLE"
 arrowtype$leads_to = "TRIANGLE"
 arrowtype$activates = "TRIANGLE"
-arrowtype$contains = "NONE"
-arrowtype$includes = "NONE"
+arrowtype$contains = "TRIANGLE"
+arrowtype$includes = "TRIANGLE"
 
 edgecolor = Object()
 edgecolor$inhibits = "0x66ff0000"
@@ -25,12 +25,26 @@ edgecolor$activates = "0x6600ff00"
 edgecolor$contains = "0x66000000"
 edgecolor$includes = "0x66000000"
 
+leg=list(list(color="0x6600ff00", shape="TRIANGLE_RIGHT", size =0.5, label= "Activates"),
+         list(color="0x66ff0000", shape="TRIANGLE_RIGHT", size =0.5, label= "Inhibits"),
+		 list(color="0x660000ff", shape="TRIANGLE_RIGHT", size =0.5, label= "Leads to"),	 
+		 list(color="0x66000000", shape="TRIANGLE_RIGHT", size =0.5, label= "Contains"),
+		 list(color="0xff0055cc", shape="CIRCLE", size =0.75, label= "Protein"),
+		 list(color="0xff0055cc", shape="SQUARE", size =0.75, label= "Protein Family"),
+		 list(color="0xff0055cc", shape="TRIANGLE_UP", size =0.75, label= "Protein Complex"),
+		 list(color="0xff0055cc", shape="DIAMOND", size =0.75, label= "Small Molecule")
+			)
+
 
 nodelist = nodes(n)
 layoutdata = cbind(nodelist,"")
 layoutdata = cbind(layoutdata,".8")
+
+
 rownames(layoutdata)=nodelist
 colnames(layoutdata)=c("nodeId","shape","size")
+entrez=nodeData(n,nodes(n),"EntrezGeneID")
+
 for(node in nodelist)
 {
 	shape=shapes[[(nodeData(n,node,"Type"))[[node]]]]
@@ -38,6 +52,34 @@ for(node in nodelist)
 		layoutdata[node,"shape"]=shape
 	if(nodeData(n,node,"Type")[[1]]=="process")
 		layoutdata[node,"size"]=.01
+	if(nodeData(n,node,"Type")[[1]]=="family"||nodeData(n,node,"Type")[[1]]=="complex")
+	{
+		linklist="start"
+		for(target in edges(n)[[node]])
+		{
+			if(edgeData(n,node,target,"edgeType")=="contains")
+			linklist=paste(linklist,nodeData(n,target,"EntrezGeneID"),sep=",")
+		}
+		linklist = sub("start,","",linklist)
+		linklist = paste("/page/DossierView/display/gene_id/",linklist,sep="")
+		entrez[[node]]=linklist
+	}
+	if(nodeData(n,node,"Type")[[1]]=="protein")
+	{
+		if(is.na(nodeData(n,target,"EntrezGeneID")))
+		{
+			entrez[[node]]=paste("/page/Search/display/?search=",node,sep="")
+		}
+		else
+		{
+			entrez[[node]]=paste("/page/Overview/display/gene_id/",nodeData(n,node,"EntrezGeneID"),sep="")
+		}
+	}
+	if(nodeData(n,node,"Type")[[1]]=="lipid")
+	{
+		entrez[[node]]=paste("/page/Search/display/?search=",node,sep="")
+	}
+	
 	
 	
 }
@@ -61,9 +103,14 @@ for(i in 1:length(edgeNames(n)))
 	adjList[i,5:6] = c(edgecolor[[interactiontype]],arrowtype[[interactiontype]])
 }
 adjList = gsub("_"," ",adjList)
-layoutdata = gsub("_"," ",layoutdata)
+layoutdata[,1] = gsub("_"," ",layoutdata[,1])
+#layoutdata = gsub("TRIANGLE UP","TRIANGLE_UP",layoutdata)
+jsEnt=toJSON(entrez)
+jsEnt=gsub("NA_character_","\"\"",jsEnt)
+jsEnt=gsub("TRIANGLE UP","TRIANGLE_UP",jsEnt)
 
 height=800
-width=933
+width=840
 
-bionetwork(adjList,list(center="FOXO",radial_labels=TRUE, layout_radialTree_startRadiusFraction=".05", layout="radialTree", height=height, width=width,padding=0,layout_data=googleDataTable(layoutdata)))
+
+bionetwork(adjList,list(center="SPRY2",radial_labels=TRUE,node_tooltips=TRUE,legend=TRUE,legend_values=leg, layout_radialTree_startRadiusFraction=".05", layout="radialTree", height=height, width=width,padding=0,layout_data=googleDataTable(layoutdata)))
